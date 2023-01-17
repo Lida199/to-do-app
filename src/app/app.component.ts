16b522f913ab09d29aeddfee7b393f3b53ec689f
Lida199 <102360930+Lida199@users.noreply.github.com>
2023-01-17 09:13:00 +0400

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'toDoApp';
+  // title = 'toDoApp';
+  taskname = "";
+  todo: any[] = [];
+  progress: any[]= [];
+  done: any[] = []; 
+  selected = "none";
+
+  createTask(){
+    if(this.taskname && this.selected !== "none"){
+      this.todo.push({body: this.taskname, difficulty: this.selected});
+      console.log(this.todo);
+      this.taskname = "";
+    }
+  };
+
+  deleteTask(index: number){
+    this.todo.splice(index,1);
+  }
+
+  toProgress(index:number){
+    this.progress.push(this.todo[index]);
+    this.todo.splice(index,1);
+  }
+  toProgress2(index:number){
+    this.progress.push(this.done[index]);
+    this.done.splice(index,1);
+  }
+
+  toTodo(index:number){
+    this.todo.push(this.progress[index]);
+    this.progress.splice(index,1);
+  }
+  toDone(index:number){
+    this.done.push(this.progress[index]);
+    this.progress.splice(index,1);
+  }
+  getItemClass(difficulty:string){
+    return{
+      ['btn-danger'] : difficulty === "Hard",
+      ["btn-success"] : difficulty === "Easy",
+      ['btn-primary'] : difficulty === "Medium",
+    }
+  }
 }
